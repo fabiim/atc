4b837cb99f2135d81e9b5f44b1fd066fb2c4069d
José Lopes <zemasa@gmail.com>
2011-11-07 03:10:53 +0000

    // TODO FABIO TRATA DISTO

--- atc/atc/GameState.java
+++ atc/atc/GameState.java
@@ -1,22 +1,341 @@
 package atc.atc;
 
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Observable;
 
-public class GameState {
+import atc.messages.Message;
+import atc.messages.NewPlane;
+import atc.messages.SetHeightGoal;
+import atc.messages.StateMessage;
+import atc.messages.Tick;
+import atc.messages.Turn;
 
-	private Map<Character,Plane> planes;
+public class GameState extends Observable {
+
+	private Map<Character,Plane> frontBuffer; // The planes being shown on the screen.
+	private Map<Character,Plane> backBuffer; // The new state of the game, still being processed.
 	private int successfulExits;
 	private int unsucessfulExits;
 	private int epoch;
+	private Board board;
 	
-	public GameState(GameState gameState) {
-		// TODO Auto-generated constructor stub
+	public Board getBoard() {
+		return board.clone();
 	}
 
-	public void processMsg(){
+	public void setBoard(Board board) {
+		this.board = board.clone();
+	}
+
+	public GameState(Map<Character, Plane> frontBuffer,
+			Map<Character, Plane> backBuffer, int successfulExits,
+			int unsucessfulExits, int epoch, Board board) {
+		this.frontBuffer = new HashMap<Character,Plane>();
+		for(Map.Entry<Character, Plane> e : frontBuffer.entrySet())
+			this.frontBuffer.put(e.getKey(), e.getValue());
 		
+		this.backBuffer = new HashMap<Character,Plane>();
+		for(Map.Entry<Character, Plane> e : backBuffer.entrySet())
+			this.backBuffer.put(e.getKey(), e.getValue());
+		this.successfulExits = successfulExits;
+		this.unsucessfulExits = unsucessfulExits;
+		this.epoch = epoch;
+		this.board = board.clone();
+	}
+
+	public GameState(GameState gameState) {
+		frontBuffer = new HashMap<Character,Plane>();
+		for(Map.Entry<Character, Plane> e : gameState.getFrontBuffer().entrySet())
+			frontBuffer.put(e.getKey(), e.getValue());
+		
+		backBuffer = new HashMap<Character,Plane>();
+		for(Map.Entry<Character, Plane> e : gameState.getBackBuffer().entrySet())
+			backBuffer.put(e.getKey(), e.getValue());
+		
+		this.successfulExits = gameState.getSuccessfulExits();
+		this.unsucessfulExits = gameState.getUnsucessfulExits();
+		this.epoch = gameState.getEpoch();
+		this.board = gameState.getBoard();
+	}
+
+	public Map<Character, Plane> getFrontBuffer() {
+		Map<Character, Plane> ret = new HashMap<Character,Plane>();
+		for(Map.Entry<Character, Plane> e : frontBuffer.entrySet())
+			ret.put(e.getKey().charValue(), e.getValue().clone());
+		return ret;
+	}
+
+	public void setFrontBuffer(Map<Character, Plane> frontBuffer) {
+		this.frontBuffer = new HashMap<Character,Plane>();
+		for(Map.Entry<Character, Plane> e : frontBuffer.entrySet())
+			this.frontBuffer.put(e.getKey().charValue(), e.getValue().clone());
+	}
+
+	public Map<Character, Plane> getBackBuffer() {
+		Map<Character, Plane> ret = new HashMap<Character,Plane>();
+		for(Map.Entry<Character, Plane> e : backBuffer.entrySet())
+			ret.put(e.getKey().charValue(), e.getValue().clone());
+		return ret;
+	}
+
+	public void setBackBuffer(Map<Character, Plane> backBuffer) {
+		this.backBuffer = new HashMap<Character,Plane>();
+		for(Map.Entry<Character, Plane> e : backBuffer.entrySet())
+			this.backBuffer.put(e.getKey().charValue(), e.getValue().clone());
+	}
+
+	public int getSuccessfulExits() {
+		return successfulExits;
+	}
+
+	public void setSuccessfulExits(int successfulExits) {
+		this.successfulExits = successfulExits;
+	}
+
+	public int getUnsucessfulExits() {
+		return unsucessfulExits;
+	}
+
+	public void setUnsucessfulExits(int unsucessfulExits) {
+		this.unsucessfulExits = unsucessfulExits;
+	}
+
+	public int getEpoch() {
+		return epoch;
+	}
+
+	public void setEpoch(int epoch) {
+		this.epoch = epoch;
+	}
+
+	public <T extends Message> void processMsg(T msg){
+		switch(msg.getOpcode()){
+			case TICK:
+				processTick((Tick) msg);
+				break;
+			case STATEMESSAGE:
+				processStateMsg((StateMessage) msg);
+				break;
+			case TURN:
+				processTurn((Turn) msg);
+				break;
+			case SET_HEIGHT_GOAL:
+				processHeightGoal((SetHeightGoal) msg);
+				break;
+			case NEW_PLANE:
+				processNewPlane((NewPlane) msg);
+				break;
+			default:
+				break;
+		}
 	}
 	
+	private void processTick(Tick tick){
+		int x,y;
+		
+		// Fazer as operações primeiro por causa dos observers no swapBuffers()
+		// Por os aviões a marchar
+		for(Plane p : backBuffer.values())
+			p.move();
+		
+		// Remover os aviões que saem do mapa
+		boolean succ = false;
+		for(Plane p : backBuffer.values()){ // A diferença entre tirar logo quando ele chega na porta ou tirar depois de ter passado um epoch na porta
+			x = p.getxCoord();
+			y = p.getyCoord();
+			
+			if(x==0 || y==0 || y==board.getHeight() || x==board.getWidth()){
+				for(Gate g : board.getPorts().values())
+					if(g.getxCoord()==x && g.getyCoord()==y && p.getExitAltitude()==p.getAltitude()){
+						this.successfulExits++;
+						succ=true;
+						break;
+					}
+				if(succ)
+					succ=false;
+				else
+					this.unsucessfulExits++;	
+				backBuffer.remove(p.getID());
+			}
+		}
+		
+		// Remover aviões que colidiram no epoch anterior - estão marcados com um '+'
+		for(Plane p : frontBuffer.values())
+			if(p.getSymbol()=='+'){
+				backBuffer.remove(p.getID());
+				this.unsucessfulExits++;
+			}
+			
+		// Testar colisões
+		for(Plane p : backBuffer.values()){
+			x = p.getxCoord();
+			y = p.getyCoord();
+			for(Plane p2 : backBuffer.values()){
+				int x2 = p2.getxCoord();
+				int y2 = p2.getyCoord();
+				if(p!=p2 && Math.abs(x-x2)<=1 && Math.abs(y-y2)<=1 && Math.abs(p.getAltitude().getHeight()-p2.getAltitude().getHeight())<=1000){
+					p.setSymbol('+');
+					p2.setSymbol('+');
+				}// Não fazer 'continue;' pq pode haver 3 ou mais aviões a colidirem
+			}
+		}
+		
+		// Trocar os buffers
+		swapBuffers();
+	}
+
+	private void processStateMsg(StateMessage statemsg){
+
+	}
+
+	private void processTurn(Turn turn){
+		backBuffer.get(turn.getPlaneId()).setDirection(turn.getDirection());
+	}
+
+	private void processHeightGoal(SetHeightGoal goal){
+		backBuffer.get(goal.getPlaneId()).setAltitudeObjectivo(goal.getObjectiveHeight());
+	}
+
+	private void processNewPlane(NewPlane plane){
+		char id = plane.getEntranceGateId();
+		
+		// Verificar se há "vaga" para um novo avião
+		if(backBuffer.keySet().size()>=26) // Só podem haver no máximo 26 aviões
+			return;
+		
+		// Onde está a porta?
+		int x = board.getPorts().get(id).getxCoord();
+		int y = board.getPorts().get(id).getyCoord();
+		
+		// Verificar se já não foi adicionado um avião aquela porta neste epoch
+		for(Plane p : backBuffer.values())
+			if(p.getxCoord()==x && p.getyCoord()==y)
+				return;
+		
+		// Verificar se não foi adicionado um avião naquela porta com aquela altitude no epoch anterior
+		for(Plane p : frontBuffer.values())
+			if(p.getxCoord()==x && p.getyCoord()==y && p.getAltitude()==plane.getHeight())
+				return;
+		
+		// OK, bora lá adicionar um avião então!
+		char newID;
+		for(newID='A'; newID<='Z'; newID++)
+			if(!backBuffer.containsKey(newID))
+				break;
+		
+		Plane p = new Plane(newID, board.getPorts().get(id).clone(), plane.getHeighGoal(),
+				plane.getHeight(), plane.getHeight(), x, y,
+				plane.getDirection(), newID, 1);
+		
+		backBuffer.put(newID, p);		
+	}
+	
+	synchronized public void swapBuffers(){
+		Map<Character,Plane> ret = new HashMap<Character,Plane>();
+		frontBuffer = new HashMap<Character,Plane>();
+		for(Map.Entry<Character, Plane> e : backBuffer.entrySet()){
+			char key = e.getKey().charValue();
+			Plane p = e.getValue();
+			frontBuffer.put(key,p.clone());
+			ret.put(key, p.clone());
+		}
+		setChanged();
+		notifyObservers(ret); 	
+	}
+	
+	@Override
+	public String toString() {
+		return "GameState [frontBuffer=" + frontBuffer + ", backBuffer="
+				+ backBuffer + ", successfulExits=" + successfulExits
+				+ ", unsucessfulExits=" + unsucessfulExits + ", epoch=" + epoch
+				+ ", board=" + board + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((backBuffer == null) ? 0 : backBuffer.hashCode());
+		result = prime * result + epoch;
+		result = prime * result
+				+ ((frontBuffer == null) ? 0 : frontBuffer.hashCode());
+		result = prime * result + successfulExits;
+		result = prime * result + unsucessfulExits;
+		result = prime * result + board.hashCode();
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GameState other = (GameState) obj;
+		if (backBuffer == null) {
+			if (other.backBuffer != null)
+				return false;
+		} else if (!backBuffer.equals(other.backBuffer))
+			return false;
+		if (epoch != other.epoch)
+			return false;
+		if (frontBuffer == null) {
+			if (other.frontBuffer != null)
+				return false;
+		} else if (!frontBuffer.equals(other.frontBuffer))
+			return false;
+		if (successfulExits != other.successfulExits)
+			return false;
+		if (unsucessfulExits != other.unsucessfulExits)
+			return false;
+		
+		// Compare frontBuffer
+		Map<Character, Plane> o = other.getFrontBuffer();
+		if(frontBuffer.size()!=o.size()) return(false);
+		Iterator<Character> it = frontBuffer.keySet().iterator();
+		Iterator<Character> it2 = o.keySet().iterator();
+		char c1,c2;
+		while(it.hasNext()){
+			c1 = it.next();
+			c2 = it2.next();
+			if(c1!=c2 || !frontBuffer.get(c1).equals(o.get(c2)))
+				return(false);
+		}
+		
+		// Compare backBuffer	
+		o = other.getBackBuffer();
+		if(backBuffer.size()!=o.size()) return(false);
+		it = backBuffer.keySet().iterator();
+		it2 = o.keySet().iterator();
+		while(it.hasNext()){
+			c1 = it.next().charValue();
+			c2 = it2.next().charValue();
+			if(c1!=c2 || !backBuffer.get(c1).equals(o.get(c2)))
+				return(false);
+		}
+		
+		// Compare board
+		Board b = other.getBoard();
+		if(b.getHeight()!=board.getHeight() || 
+				b.getWidth()!=board.getWidth() ||
+				b.getPorts().size()!=board.getPorts().size())
+			return(false);
+		it = board.getPorts().keySet().iterator();
+		it2 = b.getPorts().keySet().iterator();
+		while(it.hasNext()){
+			c1 = it.next().charValue();
+			c2 = it.next().charValue();
+			if(c1!=c2 || !b.getPorts().get(c1).equals(board.getPorts().get(c2)))
+				return(false);
+		}
+		
+		return true;
+	}
+
 	public GameState clone(){ return new GameState(this); }  
 }
--- atc/atc/Plane.java
+++ atc/atc/Plane.java
@@ -4,21 +4,21 @@ public class Plane {
 
 	private Character ID;
 	private Gate exitGate;
-	private int exitAltitude;
-	private int altitudeObjectivo;
-	private int altitude;
+	private PlaneHeight exitAltitude;
+	private PlaneHeight altitudeObjectivo;
+	private PlaneHeight altitude;
 	private int xCoord;
 	private int yCoord;
-	private int direction;
+	private PlaneDirection direction;
 	private char symbol;
 	private int speed; // Casas/Tick
 	
 	
 	
 	
-	public Plane(Character iD, Gate exitGate, int exitAltitude,
-			int altitudeObjectivo, int altitude, int xCoord, int yCoord,
-			int direction) {
+	public Plane(Character iD, Gate exitGate, PlaneHeight exitAltitude,
+			PlaneHeight altitudeObjectivo, PlaneHeight altitude, int xCoord, int yCoord,
+			PlaneDirection direction) {
 		super();
 		ID = iD;
 		this.exitGate = exitGate.clone();
@@ -32,9 +32,9 @@ public class Plane {
 		speed = 1;
 	}
 	
-	public Plane(Character iD, Gate exitGate, int exitAltitude,
-			int altitudeObjectivo, int altitude, int xCoord, int yCoord,
-			int direction, char symbol, int speed) {
+	public Plane(Character iD, Gate exitGate, PlaneHeight exitAltitude,
+			PlaneHeight altitudeObjectivo, PlaneHeight altitude, int xCoord, int yCoord,
+			PlaneDirection direction, char symbol, int speed) {
 		super();
 		ID = iD;
 		this.exitGate = exitGate.clone();
@@ -64,27 +64,27 @@ public class Plane {
 		this.exitGate = exitGate.clone();
 	}
 
-	public int getExitAltitude() {
+	public PlaneHeight getExitAltitude() {
 		return exitAltitude;
 	}
 
-	public void setExitAltitude(int exitAltitude) {
+	public void setExitAltitude(PlaneHeight exitAltitude) {
 		this.exitAltitude = exitAltitude;
 	}
 
-	public int getAltitudeObjectivo() {
+	public PlaneHeight getAltitudeObjectivo() {
 		return altitudeObjectivo;
 	}
 
-	public void setAltitudeObjectivo(int altitudeObjectivo) {
+	public void setAltitudeObjectivo(PlaneHeight altitudeObjectivo) {
 		this.altitudeObjectivo = altitudeObjectivo;
 	}
 
-	public int getAltitude() {
+	public PlaneHeight getAltitude() {
 		return altitude;
 	}
 
-	public void setAltitude(int altitude) {
+	public void setAltitude(PlaneHeight altitude) {
 		this.altitude = altitude;
 	}
 
@@ -104,11 +104,11 @@ public class Plane {
 		this.yCoord = yCoord;
 	}
 
-	public int getDirection() {
+	public PlaneDirection getDirection() {
 		return direction;
 	}
 
-	public void setDirection(int direction) {
+	public void setDirection(PlaneDirection direction) {
 		this.direction = direction;
 	}
 
@@ -133,10 +133,10 @@ public class Plane {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((ID == null) ? 0 : ID.hashCode());
-		result = prime * result + altitude;
-		result = prime * result + altitudeObjectivo;
-		result = prime * result + direction;
-		result = prime * result + exitAltitude;
+		result = prime * result + altitude.ordinal();
+		result = prime * result + altitudeObjectivo.ordinal();
+		result = prime * result + direction.ordinal();
+		result = prime * result + exitAltitude.ordinal();
 		result = prime * result
 				+ ((exitGate == null) ? 0 : exitGate.hashCode());
 		result = prime * result + symbol;
@@ -199,11 +199,50 @@ public class Plane {
 			 direction, symbol, speed));
 	}
 	
+	// TODO pra que que isto serve?
 	public void turn(){
 		
 	}
 
 	public void move(){
-		// Trata tb da altura
+		// Altura
+		if(altitude.getHeight()<altitudeObjectivo.getHeight()) // Aqui metes 1000
+			// TODO FABIO TRATA DISTO
+		if(altitude.getHeight()>altitudeObjectivo.getHeight()) // Aqui tiras 1000
+			// TODO FABIO TRATA DISTO
+	
+		// Andar
+		switch(direction){
+			case N:
+				yCoord++;
+				break;
+			case NE:
+				yCoord++;
+				xCoord++;
+				break;
+			case E:
+				xCoord++;
+				break;
+			case SE:
+				xCoord++;
+				yCoord--;
+				break;
+			case S:
+				yCoord--;
+				break;
+			case SW:
+				xCoord--;
+				yCoord--;
+				break;
+			case W:
+				xCoord--;
+				break;
+			case NW:
+				xCoord--;
+				yCoord++;
+				break;
+			default:
+				break;
+		}	
 	}
 }
